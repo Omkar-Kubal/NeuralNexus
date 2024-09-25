3df2f027405ba6b8d685a3ee5453cb6f7aff68be
Omkar Santosh Kubal <163621339+Omkar-Kubal@users.noreply.github.com>
2024-09-25 14:53:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//This is single line comment
+/* This is a multi-line comment */
+
+/* Data Types: undefined, null, string, symbol, number and object */
+var myName = "Beau"
+myName = 8
+let ourName = "freecodecamp"
+const pi = 3.14
+
+// declaring variable
+var a;
+// assigning variable
+var a = 2;
+b = a;
+// console.log-allows to see in console
+console.log(a)
+// initialize variable when creating it
+var a=2;
+
+// length-to find length
+var firstName = 'Ada';
+firstName.length;
+
+// Array
+var ourArray = ["John", 23];
+// 2-D Array
+var ourArray = [["the universe",42], ["everything",101011]]
